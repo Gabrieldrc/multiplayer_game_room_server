73982e0a472414aaf94590d4e286109a7286583a
Gabrieldrc <gabriel.drc25@gmail.com>
2022-02-02 16:18:44 -0300

    SVR-0003 mongo initializer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { EventsModule } from './events/events.module';
 import { GamesModule } from './games/games.module';
 import { UtilsModule } from './utils/utils.module';
 import { CoreModule } from './core/core.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.development.env' }),
+    MongooseModule.forRoot('mongodb://root:example@127.0.0.1:27017/admin'),
     EventsModule,
     GamesModule,
     UtilsModule,
